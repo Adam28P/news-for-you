5c9f78497ca3c64bc5228c40dab9545da025c679
Adam P <adamparalovos@hotmail.com>
2018-11-06 00:15:47 -0500

    Added controller.js content

---
+++ controller/controller.js
@@ -0,0 +1,19 @@
+//dependencies
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+//require request and cheerio to scrape
+var request = require('request');
+var cheerio = require('cheerio');
+
+//Require models
+var Comment = require('../models/Comment.js');
+var Article = require('../models/Article.js');
+
+//index
+router.get('/', function(req, res) {
+    res.redirect('/articles');
+});
+
+module.exports = router;
